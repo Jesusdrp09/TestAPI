213eede9084c5f206d8f40ebf7d43d543cf1de1a
Jesus David Rios <jesus.rios+rappinc@rappi.com>
2025-11-29 23:59:28 -0500

    First Commit

---
+++ FLASK API/Flask/test.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/get")
+def get():
+    return f"{request.args["username"]}"
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "GET":
+        return render_template('form.html')
+    elif request.method == "POST":
+        return f"Tu nombre es {request.form["name"]}"
+    
+@app.route("/login", methods=["GET", "POST"])
+def register(): 
+    if request.method == "GET":
+        return render_template('login.html')
+    elif request.method == "POST":
+        return f"{request.form["username"]}, {request.form["password"]}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
