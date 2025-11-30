ec24af5ea3204e5f6b401199d0e33de2f0631221
Jesus David Rios <jesus.rios+rappinc@rappi.com>
2025-11-30 00:12:52 -0500

    Improving for render

--- FLASK API/Flask/test.py
+++ FLASK API/Flask/test.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import os 
 
 app = Flask(__name__)
 
@@ -21,4 +22,4 @@ def register():
         return f"{request.form["username"]}, {request.form["password"]}"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=os.environ.get("PORT", 5000), debug=True)
